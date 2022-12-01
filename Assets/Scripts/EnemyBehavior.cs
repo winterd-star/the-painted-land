17247e997cdd89754885af1e0ab01e129da0bcd6
Winter <winterpony4@gmail.com>
2022-12-01 09:28:53 -0600

    game borked
    freezes on pressing play

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : CharacterController2D
+{
+    public float speed;
+    Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+
+        if(faceRight())
+        {
+            rigidBody.velocity = new Vector2(speed, 0f);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(-speed, 0f);
+        }
+
+    }
+
+
+    private bool faceRight()
+    {
+        return transform.localScale.x > 0.001f;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) //turning
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(rigidBody.velocity.x)), transform.localScale.y);
+    }
+}
--- Assets/Scripts/PlatformerPlayerCharacter2D.cs
+++ Assets/Scripts/PlatformerPlayerCharacter2D.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class PlatformerPlayerCharacter2D : CharacterController2D
 {
-    public float speed = 10.0f;
+    public float speed = 3.0f;
     public float collisionTestOffset;
     public SpriteRenderer spriteRenderer;
 
     private Rigidbody2D rigidbody2D;
+    private bool canJumpAgain;
     private float jumpInputLastFrame = 0.0f;
 
     // Start is called before the first frame update
@@ -27,9 +28,19 @@ public class PlatformerPlayerCharacter2D : CharacterController2D
         // Test before you move
         if (xInput != 0.0f)
             motion.x = xInput*speed;
+
+        while (IsTouchingGround())
+        {
+            canJumpAgain = true;
+        }
         
         if (Input.GetAxis("Jump") > 0.0f && isTouchingGround){
-            motion.y = speed+2.5f;
+                motion.y = speed + 1.5f;
+            if(Input.GetAxis("Jump") > 0.0f && canJumpAgain == true)
+            {
+                motion.y = speed + 1.5f;
+                canJumpAgain = false;
+            }
         }
 
         rigidbody2D.velocity = motion;
